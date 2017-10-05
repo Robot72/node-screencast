8505d97e1566cad5f7177e82762694440035032b
Robert Kuznetsov <robot3@xaker.ru>
2017-10-05 16:03:20 +0300

    Work with modules.

--- 6-work-with-module/server.js
+++ 6-work-with-module/server.js
@@ -1,10 +1,13 @@
 let user = require('./user');
+let db = require('db');
 
 function run() {
 	let vasya = new user.User('Vasya');
 	let petr = new user.User('Petr');
 
 	petr.hello(vasya);
+
+	console.log(db.getPhrase('Run successful'))
 }
 
 if (module.parent) {
---
+++ 6-work-with-module/user/index.js
@@ -0,0 +1,16 @@
+let db = require('db');
+let log = require('logger')(module);
+db.connect();
+
+class User {
+	constructor(name) {
+		this.name = name
+	}
+
+	hello(who) {
+		log(db.getPhrase('Hello') + ', ' + who.name);
+	}
+}
+
+exports.User = User;
+log('user.js is loaded!');
